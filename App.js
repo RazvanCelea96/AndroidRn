9a7f145e3ea9a17e960708d561809e725a4df9ff
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-14 22:39:06 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+  },
+  {
+    initialRouteName: 'Search', 
+    defaultNavigationOptions: {
+      title: 'Manele Search',
+    },
+  },
+);
+
+export default createAppContainer(navigator);
---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <TextInput
+      autoCapitalize="none"
+      autoCorrect={false}
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+  },
+});
+
+export default SearchBar;
