77f3a4fbc9c94e49004060d885b9002d1b265a30
RazvanCelea96 <54838433+RazvanCelea96@users.noreply.github.com>
2020-09-15 15:49:12 +0300

    hook separation

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,36 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState([]);
-
-  const searchApi = async (searchTerm) => {
-    console.log('Hi There')
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'los angeles',
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-//Call searchApi when component
-//is first rendered
-// searchApi('pasta')
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={ () => searchApi(term)} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text> We have found {results.length} results</Text>
     </View>
